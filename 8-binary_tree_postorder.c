b846a287c3f73ba7f0d95850bec41e74f4c4bed9
Helvlaska <ccastan@live.fr>
2025-04-09 23:01:07 +0200

    :hammer: (8-binary_tree_postorder.c) : Correction betty syntax

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,7 +3,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using 
+ * binary_tree_postorder - goes through a binary tree using
  * post-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
@@ -12,13 +12,13 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    /*vérifie si l'arbre est vide et si la fonction est null*/
-    if (tree == NULL || func == NULL)
-        return; /*si echec retourne rien*/
+	/*vérifie si l'arbre est vide et si la fonction est null*/
+	if (tree == NULL || func == NULL)
+		return; /*si echec retourne rien*/
 
-    /*on parcours (recursive) le bras Gauche & on appelle func*/
-    binary_tree_postorder(tree->left, func);
-    /*on parcours (recursive) le bras Droit & on appelle func*/
-    binary_tree_postorder(tree->right, func);
-    func(tree->n); /*on appelle la fonction sur le 1e noeud (root)*/
+	/*on parcours (recursive) le bras Gauche & on appelle func*/
+	binary_tree_postorder(tree->left, func);
+	/*on parcours (recursive) le bras Droit & on appelle func*/
+	binary_tree_postorder(tree->right, func);
+	func(tree->n); /*on appelle la fonction sur le 1e noeud (root)*/
 }
