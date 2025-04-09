c68417fb2adc9c522ad5357dce3e5522adfc88e0
Helvlaska <ccastan@live.fr>
2025-04-09 17:01:42 +0200

    :sparkles: (13-binary_tree_nodes.c) : Program counts the nodes with at least 1 child in a binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of nodes
+ *
+ * Return: 0 if tree is NULL, else return the number of nodes with at least 1 child
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	/*compteur pour stocker le nombre de noeud*/
