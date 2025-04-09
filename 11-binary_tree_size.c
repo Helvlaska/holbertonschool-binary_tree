ac4fdd2758ceac790828b6bbd26b95512eeab90b
Helvlaska <ccastan@live.fr>
2025-04-09 23:13:06 +0200

    :hammer: (11-binary_tree_size.c) : Correction betty syntax

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,19 +10,19 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    /*compteur qui stock le nombre de noeud de l'arbre*/
-    size_t total_node = 0, total_left = 0, total_right = 0;
+	/*compteur qui stock le nombre de noeud de l'arbre*/
+	size_t total_node = 0, total_left = 0, total_right = 0;
 
-    /*verifie si l'arbre est vide*/
-    if (tree == NULL)
-        return (0); /*si echec retourne 0*/
-    
-    /*on avance dans l'arbre gauche et incrémente le compteur*/
+	/*verifie si l'arbre est vide*/
+	if (tree == NULL)
+		return (0); /*si echec retourne 0*/
+
+	/*on avance dans l'arbre gauche et incrémente le compteur*/
 	total_left = binary_tree_size(tree->left);
 	/*on avance dans l'arbre droite et incrémente le compteur*/
 	total_right = binary_tree_size(tree->right);
-    /*on additionne le côté gauche et le côté droite + le current node*/
-    total_node = (total_left + total_right + 1);
+	/*on additionne le côté gauche et le côté droite + le current node*/
+	total_node = (total_left + total_right + 1);
 
-    return (total_node);
+	return (total_node);
 }
