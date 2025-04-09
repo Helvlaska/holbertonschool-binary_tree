7390c6c79283c7ac35980a49052a5325efd0098c
Helvlaska <ccastan@live.fr>
2025-04-09 23:08:26 +0200

    :hammer: (9-binary_tree_height.c) : Correction betty syntax

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -19,13 +19,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	/*verifie si le noeud est une feuille*/
 	if (tree->left == NULL && tree->right == NULL)
 		return (0); /*il est à l'étage 0 on retourne 0*/
-	else
-	{
-		/*on avance dans l'arbre gauche et incrémente le compteur*/
-		left_height = binary_tree_height(tree->left);
-		/*on avance dans l'arbre droite et incrémente le compteur*/
-		right_height = binary_tree_height(tree->right);
-	}
+
+	/*on avance dans l'arbre gauche et incrémente le compteur*/
+	left_height = binary_tree_height(tree->left);
+	/*on avance dans l'arbre droite et incrémente le compteur*/
+	right_height = binary_tree_height(tree->right);
+
 	/*on compare les hauteurs gauche et droite*/
 	/*ex : [098] = return([402]+[128]+1) soit return(0+1+1) = 2 = [098]*/
 	if (left_height > right_height)
